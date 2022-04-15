368119b5633fb426d0d046c8bc3ad2bf9362483c
Aymen <aymenadvance@gmail.com>
2022-04-15 18:16:18 +0200

    move on root folder

--- src/common/GlobalVariable.js
+++
@@ -1,7 +0,0 @@
-export const API_BASE = "https://www.breakingbadapi.com/api";
-export const API_EPISODES = API_BASE.concat("/episodes");
-export const API_CHARACTER = API_BASE.concat("/characters");
-export const API_QUOTES = API_BASE.concat("/quotes");
-
-
-
