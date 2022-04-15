d5b29a7c436941139e44f8e6bf975d2b41a88ff4
Aymen <aymenadvance@gmail.com>
2022-04-15 18:13:05 +0200

    publish build

---
+++ build/precache-manifest.a17a0292e70131e4c372f8fc0e12af2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65ebaad4e6e19bb92f7",
+    "url": "/static/css/main.5f1a65f6.chunk.css"
+  },
+  {
+    "revision": "a65ebaad4e6e19bb92f7",
+    "url": "/static/js/main.52891315.chunk.js"
+  },
+  {
+    "revision": "ef5a25257b2020ef0dc6",
+    "url": "/static/js/runtime~main.664399db.js"
+  },
+  {
+    "revision": "5085c5fb9bb768a5a87b",
+    "url": "/static/js/2.e6da6c8c.chunk.js"
+  },
+  {
+    "revision": "b61d7e105384ced4ef5b",
+    "url": "/static/js/3.66188ad7.chunk.js"
+  },
+  {
+    "revision": "06a15f35ee814e571364612385e7070c",
+    "url": "/index.html"
+  }
+];
